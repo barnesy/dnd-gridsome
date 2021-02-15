d4c22125ead34e43edfa6e7826eb3d37fe778984
barnesy <chris@barnesy.me>
2021-02-15 02:16:55 -0500

    Worps had to move stuffs

---
+++ src/.temp/config.js
@@ -0,0 +1,8 @@
+export default {
+  "trailingSlash": true,
+  "pathPrefix": "/dnd-gridsome",
+  "titleTemplate": "%s - DnD5",
+  "siteUrl": "https://dnd5.barnesy.me",
+  "version": "0.7.23",
+  "catchLinks": true
+}
